a9d49a1db895da033c333540bc9f284834116748
Pavel Paroulek <pparoulek@frinx.io>
2020-07-15 15:04:13 +0200

    Adding rbac call to distinguish users
    Adding rbac call and adjusting endpoint paths for calling proxy

--- src/index.js
+++ src/index.js
@@ -5,17 +5,32 @@ import WorkflowApp from "frinx-workflow-ui/lib/App";
 import WorkflowServiceApp from "frinx-workflow-ui/lib/ServiceUiApp";
 import { HttpClient as http } from "frinx-workflow-ui/lib/common/HttpClient";
 
-const conductorApiUrlPrefix = "/workflow";
-const conductorRbacApiUrlPrefix = conductorApiUrlPrefix + "/rbac";
+const conductorApiUrlPrefix = "/workflow/proxy/proxy/api";
+const conductorRbacApiUrlPrefix = "/workflow/proxy/rbac/editableworkflows";
 const frontendUrlPrefix = "/workflow/frontend";
 
-// TODO invoke /editableworkflows and return WorkflowServiceApp in case of "false"
-// just like in magma/symphony/app/fbcnms-projects/hub/app/components/Main.js
-ReactDOM.render(
-  <WorkflowApp
-    backendApiUrlPrefix={conductorApiUrlPrefix}
-    frontendUrlPrefix={frontendUrlPrefix}
-    enableScheduling={true}
-  />,
-  document.getElementById("root")
-);
+http.get(conductorRbacApiUrlPrefix).then(isPrivileged => {
+  let elementToRenderInto = document.getElementById("root");
+
+  if (isPrivileged) {
+    ReactDOM.render(
+        <WorkflowApp
+            backendApiUrlPrefix={conductorApiUrlPrefix}
+            frontendUrlPrefix={frontendUrlPrefix}
+            enableScheduling={true}
+        />,
+        elementToRenderInto
+    );
+  } else {
+    ReactDOM.render(
+        <WorkflowServiceApp
+            backendApiUrlPrefix={conductorApiUrlPrefix}
+            frontendUrlPrefix={frontendUrlPrefix}
+            enableScheduling={true}
+        />,
+        elementToRenderInto
+    );
+  }
+});
+
+
