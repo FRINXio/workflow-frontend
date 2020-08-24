7976d1c63b852f77d95a665c388512ccd016cffb
Maros Marsalek <mmarsalek@frinx.io>
2020-08-24 09:44:38 +0200

    WF: Fix wrong backend URL in workflow UI by overriding global const
    Issue: NH-276
    Test Plan: Manually verify the UI by executing a workflow
    
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import WorkflowApp from "frinx-workflow-ui/lib/App";
+import {globalConstants} from "frinx-workflow-ui/lib/common/GlobalContext";
 import WorkflowServiceApp from "frinx-workflow-ui/lib/ServiceUiApp";
-import { HttpClient as http } from "frinx-workflow-ui/lib/common/HttpClient";
+import {HttpClient as http} from "frinx-workflow-ui/lib/common/HttpClient";
 
 // This is used to determine whether a user has admin rights
 const conductorRbacApiUrlPrefix = "/workflow/proxy/rbac/editableworkflows";
@@ -19,17 +20,35 @@ http.get(conductorRbacApiUrlPrefix).then(isPrivileged => {
   let elementToRenderInto = document.getElementById("root");
 
   if (isPrivileged) {
+    // FIXME this is redundant since the same is propagated via WorkflowApp.props
+    // However, if we don't override the globalConstants, there is an issue in workflow-ui
+    // where: After executing a workflow from def view, and clicking on the executed workflow ID
+    // the UI redirects you to the exec tab, but all of a sudden uses backendApiUrlPrefix from globalConstants
+    // instead of the one passed via properties into ContextProvider
+    globalConstants.backendApiUrlPrefix = conductorApiUrlPrefix;
+    globalConstants.frontendUrlPrefix = frontendUrlPrefix;
+    globalConstants.disabledTasks = disabledTasks;
+    globalConstants.enableScheduling = enableScheduling;
+    globalConstants.prefixHttpTask = prefixHttpTask;
+
     ReactDOM.render(
         <WorkflowApp
             backendApiUrlPrefix={conductorApiUrlPrefix}
             frontendUrlPrefix={frontendUrlPrefix}
             enableScheduling={enableScheduling}
             disabledTasks={disabledTasks}
-            prefixHttpTask={prefixHttpTask}
-        />,
+            prefixHttpTask={prefixHttpTask}>
+        </WorkflowApp>,
         elementToRenderInto
     );
   } else {
+      // FIXME redundant, same as above
+    globalConstants.backendApiUrlPrefix = conductorApiRbacProxyUrlPrefix;
+    globalConstants.frontendUrlPrefix = frontendUrlPrefix;
+    globalConstants.disabledTasks = disabledTasks;
+    globalConstants.enableScheduling = false;
+    globalConstants.prefixHttpTask = prefixHttpTask;
+
     ReactDOM.render(
         <WorkflowServiceApp
             backendApiUrlPrefix={conductorApiRbacProxyUrlPrefix}
